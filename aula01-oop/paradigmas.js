a7b8800fffc721db1f72e83caabf98874ead6518
Supervisor <supervisor@cmsm.local>
2022-03-08 13:07:41 -0300

    aula01

---
+++ aula01-oop/paradigmas.js
@@ -0,0 +1,8 @@
+console.log("===PARADIGMA===")
+
+const carro = {};
+carro.cor = "Vermelho";
+carro["marca"] = "WV";
+
+
+console.log(carro)
---
+++ ex01-oop/script.js
@@ -0,0 +1,33 @@
+console.log('===EX01-OOP===')
+
+const pessoa = {}
+
+pessoa['cor da pele'] = 'Preta'
+pessoa['cor dos olhos'] = 'Preto'
+pessoa['cor do cabelo'] = 'Preto'
+pessoa.sexo = 'Masculino'
+pessoa.idade = 22
+pessoa.peso = 75
+pessoa.altura = 1.75
+
+pessoa.comer = function () {
+    return 'Comendo!!!'
+}
+pessoa.dormir = function () {
+    return 'Dormindo!!!'
+}
+pessoa.acordar = function () {
+    return 'Acordando!!!'
+}
+pessoa.andar = function () {
+    return 'Andando!!!'
+}
+pessoa.correr = function () {
+    return 'Correndo!!!'
+}
+
+console.log(pessoa)
+console.log(pessoa['cor da pele'])
+console.log(pessoa.idade)
+console.log(pessoa.comer())
+console.log(pessoa['correr']())
